5dd87a7615a658e8a6fd9e63c86bc87778d4da33
Justin Martinez <3260193+jlmart88@users.noreply.github.com>
2019-06-03 20:02:14 -0700

    actually fix build runtime

--- tsconfig-paths-bootstrap.js
+++ tsconfig-paths-bootstrap.js
@@ -1,7 +1,11 @@
-const tsConfig = require("./tsconfig.json");
+require('jsonminify');
+const fs = require('fs');
+// minify the tsconfig.json before parsing because it has comments in it
+const tsConfig = JSON.parse(JSON.minify(fs.readFileSync('./tsconfig.json', { encoding: 'utf8' })));
 const tsConfigPaths = require("tsconfig-paths");
 
 tsConfigPaths.register({
+  // override the baseUrl with the outDir, because that is where we compiled the files to with tsc
   baseUrl: tsConfig.compilerOptions.outDir,
   paths: tsConfig.compilerOptions.paths
 });
