2fd0c8a497c5082f73c2ee42d26a406055380852
jlmart88 <3260193+jlmart88@users.noreply.github.com>
2023-12-09 13:00:15 -0800

    add analytics and speed insights

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,3 +1,5 @@
+import { Analytics } from "@vercel/analytics/react";
+import { SpeedInsights } from "@vercel/speed-insights/next";
 import type { Metadata, Viewport } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
@@ -21,7 +23,11 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <body className={`${inter.className} bg-slate-800`}>{children}</body>
+      <body className={`${inter.className} bg-slate-800`}>
+        {children}
+        <Analytics />
+        <SpeedInsights />
+      </body>
     </html>
   );
 }
