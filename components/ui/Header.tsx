8628494ec0f395d683442670f08deac43556a0f0
jlmart88 <3260193+jlmart88@users.noreply.github.com>
2023-12-09 14:21:21 -0800

    rewrite faq page

--- components/ui/Header.tsx
+++ components/ui/Header.tsx
@@ -1,7 +1,11 @@
+import Link from "next/link";
+
 export const Header = () => {
   return (
     <div className="flex w-full flex-col items-center justify-center gap-4 p-4 text-center">
-      <h1 className="text-3xl font-medium">Kaomoji App</h1>
+      <Link href="/">
+        <h1 className="text-3xl font-medium">Kaomoji App</h1>
+      </Link>
       <h2 className="whitespace-nowrap font-mono text-lg">
         (╮°-°)╮┳━━┳ &nbsp;&nbsp;( ╯°□°)╯ ┻━━┻
       </h2>
